fedf3be04df82b3806dde75369433dc1cafd3591
Andre Leon <alejandroll.blender@gmail.com>
2026-01-29 17:27:13 +0100

    Finished hero and highlight section styling

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -5,8 +5,10 @@ function HeroSection() {
     return(
         <section className='HeroSectionContainer'>
             <div className="DescriptionContainer">
-                <h1>Little Lemon</h1>
-                <p>Chicago</p>
+                <div className="LogoAndSublogo">
+                    <h1>Little Lemon</h1>
+                    <p>Chicago</p>
+                </div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nihil id reiciendis officiis sapiente. Eaque eveniet iusto nemo, eos error, quos nostrum obcaecati ex quae, officiis aut sequi ducimus sint!</p>
                 <button>Reserve a table</button>
             </div>
--- src/HighlightSection.jsx
+++ src/HighlightSection.jsx
@@ -3,8 +3,10 @@ import "./HighlightSection.css"
 function HighlightSection({children}) {
     return(
         <section className='HighlightSectionContainer'>
-            <h1>This week's specials</h1>
-            <button>Online Menu</button>
+            <div className="TextAndButtonContainer">
+                <h1>This week's specials</h1>
+                <button>Online Menu</button>
+            </div>
             <div className="MultiCardContainer">
                 {children}
             </div>
