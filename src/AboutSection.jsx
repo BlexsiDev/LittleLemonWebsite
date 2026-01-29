0d16d67f8146b9c55a1d557eaa8969366318afcc
Andre Leon <alejandroll.blender@gmail.com>
2026-01-29 19:37:14 +0100

    About Section finished

--- src/AboutSection.jsx
+++ src/AboutSection.jsx
@@ -1,11 +1,20 @@
+import "./AboutSection.css"
+import MAA from "./assets/Mario and Adrian A.jpg"
+import RCB from "./assets/restaurant chef B.jpg"
+
 function AboutSection() {
     return(
         <section className='AboutSectionContainer'>
-            <h1>Little Lemon</h1>
-            <p>Chicago</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <img src="https://picsum.photos/280/350" alt="" width={280} height={350}/>
-            <img src="https://picsum.photos/280/350" alt="" width={280} height={350}/>
+            <div className="AboutTextContainer">
+                <h1>Little Lemon</h1>
+                <p>Chicago</p>
+                <br/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="ImgContainer">
+                <img id="BottomImage" src={RCB} alt="" width={280} height={350}/>
+                <img id="TopImage" src={MAA} alt="" width={280} height={350}/>
+            </div>
         </section>
     );
 }
