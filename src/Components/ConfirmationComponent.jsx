11bd855cd112ccf97a63e80b9785009ccbdf02d3
Andre Leon <alejandroll.blender@gmail.com>
2026-02-23 14:41:53 +0100

    Added a comfirmation screen including some assets for it and connected it all

--- src/Components/ConfirmationComponent.jsx
+++ src/Components/ConfirmationComponent.jsx
@@ -1,16 +1,26 @@
+import "./ConfirmationComponent.css"
+import checkMark from "../assets/CheckMark.png"
+import homeIcon from "../assets/home-icon.png"
 import { Link } from "react-router-dom";
 
 const ConfirmationComponent = () => {
   return (
     <>
-      <h1>Succesfully reserved a table</h1>
-      <p>Hope to see you!</p>
-      <img src="#" alt="Confirmation check image" />
-      <Link to={'/'}>
-        <button>
-            <img src="#" alt="Home Icon" />
-        </button>
-      </Link>
+      <section className="ConfirmationSection">
+        <div className="ConfirmationCard">
+          <img src={checkMark} alt="Confirmation check image" />
+          <h1>Succesfully reserved a table</h1>
+          <p>
+            Thank you for reserving a table. <br/>
+            See you soon!
+          </p>
+          <Link to={'/'}>
+            <button>
+                <img src={homeIcon} alt="Home Icon" id="homeIcon"/>
+            </button>
+          </Link>   
+        </div>
+      </section>
     </>
   )
 }
