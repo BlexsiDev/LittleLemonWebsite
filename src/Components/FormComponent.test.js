e96f7e4e39c2e29dacde9dcb1aac6965cdbdf0bb
Andre Leon <alejandroll.blender@gmail.com>
2026-02-24 18:34:30 +0100

    Added react testing library and Vitest to test

--- src/Components/FormComponent.test.js
+++ src/Components/FormComponent.test.js
@@ -1,10 +1,11 @@
 import { render, expect } from "@testing-library/react";
+import { vi, test } from "vitest";
 import FormComponent from './FormComponent';
 
 test('Initialize and updateTimes to check if they return the correct values', () => {
     render(<FormComponent />);
-    const initializeTimes = jest.fn();
-    const updateTimes = jest.fn();
+    const initializeTimes = vi.fn()
+    const updateTimes = vi.fn();
 
     expect(initializeTimes).toHaveBeenCalled();
     expect(updateTimes).toHaveBeenCalled();
