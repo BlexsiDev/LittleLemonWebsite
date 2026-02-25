adc2c4842a0e3391bb8439974ed4d014ac9332e2
Andre Leon <alejandroll.blender@gmail.com>
2026-02-25 05:28:47 +0100

    Added a sidebar (provisional) for responsiveness and other responsiveness related stuff

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,20 +1,45 @@
 import "./Header.css"
 import {Link} from 'react-router-dom'
 import logo from "./assets/LittleLemonLogo.png"
+import hamburguerIcon from "./assets/hamburguer.svg"
+import xicon from "./assets/xIcon2.png"
 
 function Header() {
     return(
         <header className='NavBarContainer'>
             <img src={logo} alt="Little Lemon logo" width={214}/>
             <nav>
-                <ul>
-                    <li><Link to="/">HOME</Link></li>
-                    <li><Link to="/">ABOUT</Link></li>
-                    <li><Link to="/">MENU</Link></li>
-                    <li><Link to="/booking">RESERVATIONS</Link></li>
-                    <li><Link to="/">ORDER ONLINE</Link></li>
-                    <li><Link to="/">LOG IN</Link></li>
-                </ul>
+                <input type="checkbox" id="sidebar-active"/>
+                <label htmlFor="sidebar-active" className="open-sidebar-button">
+                    <img src={hamburguerIcon} alt="burger icon image" />
+                </label>
+
+                <div className="links-container">
+                    <label htmlFor="sidebar-active" className="close-sidebar-button">
+                        <img src={xicon} alt="cross image" />
+                    </label>
+                    
+                    <ul>
+                        <li><Link to="/">HOME</Link></li>
+                        <li><Link to="/">ABOUT</Link></li>
+                        <li><Link to="/">MENU</Link></li>
+                        <li><Link to="/booking">RESERVATIONS</Link></li>
+                        <li><Link to="/">ORDER ONLINE</Link></li>
+                        <li><Link to="/">LOG IN</Link></li>
+                    </ul>
+                </div>
+
+                <div className="desktop-links">
+                    <ul>
+                        <li><Link to="/">HOME</Link></li>
+                        <li><Link to="/">ABOUT</Link></li>
+                        <li><Link to="/">MENU</Link></li>
+                        <li><Link to="/booking">RESERVATIONS</Link></li>
+                        <li><Link to="/">ORDER ONLINE</Link></li>
+                        <li><Link to="/">LOG IN</Link></li>
+                    </ul>
+                </div>
+                
             </nav>
         </header>
     );
